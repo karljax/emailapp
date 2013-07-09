2addde9dd21fb66799e2baf792fefb881a4431ee
Karl Jackson <karljax@gmail.com>
2013-07-08 22:36:10 -0500

    mesage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,16 @@ Emailapp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => '',
+    :authentication => :login,
+    :user_name      => '',
+    :password       => ''
+  }
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
