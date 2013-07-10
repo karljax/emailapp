166c11d946ce2524145a6449115e286480566a58
Karl Jackson <karljax@gmail.com>
2013-07-10 12:52:06 -0500

    sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,16 +14,8 @@ Emailapp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => 587,
-    :domain         => '',
-    :authentication => :login,
-    :user_name      => '',
-    :password       => ''
-  }
+  config.action_mailer.raise_delivery_errors = false
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
